ca7aa4785e37b45dcfcfff39158296aa468c4483
fc1943s <root@fc1943s.dev>
2021-03-27 23:11:24 -0300

    added since arg

--- src/Program.fs
+++ src/Program.fs
@@ -49,11 +49,18 @@ module Git2Pdf =
         pdfRenderer.RenderDocument()
         pdfRenderer.PdfDocument.Save outputPath
 
-    let startIoAsync (branch: string) (repoPath: string) (outputPath: string) =
+    let startIoAsync (branch: string) (repoPath: string) (outputPath: string) (since: string) =
         async {
             Encoding.RegisterProvider CodePagesEncodingProvider.Instance
 
-            let! commitList = runCmdIoAsync repoPath [ "git rev-list " + branch ]
+            let! commitList =
+                runCmdIoAsync
+                    repoPath
+                    [ $"""git rev-list {branch} {
+                                                     match since with
+                                                     | "" -> ""
+                                                     | since -> $"--since={since}"
+                      }""" ]
 
             match commitList with
             | None -> eprintfn "Error retrieving commit list"
@@ -68,12 +75,14 @@ module Args =
         | [<Mandatory>] Repo_Path of string
         | [<Mandatory>] Branch of string
         | [<Mandatory>] Output_Path of string
+        | Since of string
         interface IArgParserTemplate with
             member this.Usage =
                 match this with
                 | Repo_Path _ -> "Path of the repository."
                 | Branch _ -> "Branch name."
                 | Output_Path _ -> "Full path of the PDF file."
+                | Since _ -> "Initial date according to git-rev-list formatting e.g. '4.months.ago'."
 
 module Program =
     [<EntryPoint>]
@@ -84,6 +93,7 @@ module Program =
             (args.GetResult Args.Branch)
             (args.GetResult Args.Repo_Path)
             (args.GetResult Args.Output_Path)
+            (args.GetResult Args.Since)
         |> Async.RunSynchronously
 
         0
